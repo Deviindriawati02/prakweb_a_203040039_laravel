8b8a8f4c2ac9ed206f12c1fa7f08409d53dc231f
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-10-17 22:00:31 +0700

    menyelesaikan video 4

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <!--navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
       <div class="container">
         <a class="navbar-brand" href="#">Depi Cute Blog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
