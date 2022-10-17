edece6bd1d661d907afbeff4a0886c83d4c11bb3
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-10-17 19:36:52 +0700

    menyelesaikan video 3

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/Home', function () {
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about', [
+        "name" => "Devi Indriawati",
+        "email" => "Deviindriawati02@gmail.com",
+        "image" => "dep.jpeg"
+
+    ]);
+});
+
+Route::get('/posts', function () {
+    return view ('posts');
+});
