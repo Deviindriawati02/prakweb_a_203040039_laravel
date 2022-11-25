735633abb8c1eb81494c45bb6b242e7a13cb58ae
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-11-26 00:06:35 +0700

    Menyelesaikan video 20 update & Delete Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -14,7 +14,7 @@
   <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
   <table class="table table-striped table-sm">
     <thead>
-      <tr>
+      <tr>   
         <th scope="col">#</th>
         <th scope="col">Title</th>
         <th scope="col">Category</th>
@@ -29,8 +29,12 @@
         <td>{{$post->category->name}}</td>
         <td>
           <a href="/dashboard/posts/{{$post->slug}}" class="badge bg-info"><span data-feather="eye"></span></a>
-          <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-          <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+          <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+          <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+            @method('delete')
+            @csrf 
+            <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')" ><span data-feather="x-circle"></span></button>
+          </form>
         </td>
       </tr>
       @endforeach
